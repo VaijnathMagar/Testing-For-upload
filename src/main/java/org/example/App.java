09d57f6782726c0e94ede69ad6c6bf2cc8d6484b
vaijnathmagar <vaijnathmagar5@gmail.com>
2023-02-16 12:44:32 +0530

    intinal vesion

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+
+        System.out.println( "Hello World!" );
+
+        Student stu = new Student();
+        stu.setName("Vaijnat ");
+
+        System.out.println(stu);
+    }
+
+
+
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+public class Student {
+
+    private String name;
+
+}
